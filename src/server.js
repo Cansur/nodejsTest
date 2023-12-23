00ae8c16dae7c365382c275fcd726241165b04f3
Cansur <tjswo0682@gmail.com>
2023-12-23 15:27:39 +0900

    mysql 연동

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const db = require('../src/tmp/db.js');
+
 const app = express();
 
+
+// configuration =================================================================
 app.listen(8080, function(){
     console.log('listening on port 8080');
 });
@@ -8,6 +12,16 @@ app.listen(8080, function(){
 app.get('/', function(요청, 응답){
     응답.sendFile(__dirname + '/index.html');
 });
+
+app.get('/users', (req, res)=>{
+    db.query('SELECT * FROM users', function(err, results, fields) {
+        if(err) console.error(err);
+        console.log(results);
+        res.json(results);
+    });
+});
+
+
 
 
 // 간편 시작하기
