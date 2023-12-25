da5e3c0e813d2d8d216983bb30ffe138b85d2793
Cansur <tjswo0682@gmail.com>
2023-12-26 01:35:22 +0900

    ejs 처음 시도

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 const express = require('express');
-const db = require('../src/tmp/db');
+const fs = require('fs');
 
 // Routers
 const usersRoutes = require('./router/users');
+const db = require('./router/db');
 
 // express
 const app = express();
+
+// ejs
+app.set('view engine', 'ejs');
 
 // configuration =================================================================
 app.listen(8080, function(){
@@ -16,20 +20,22 @@ app.get('/', function(요청, 응답){
     응답.sendFile(__dirname + '/index.html');
 });
 
-// app.get('/users', (req, res)=>{
-//     res.sendFile(__dirname + '/tmp/users.html');
-//     db.query('SELECT * FROM users', function(err, results, fields) {
-//         if(err) console.error(err);
-//         console.log(results);
-//         // res.json(results);
-//     });
-// });
-
 app.get('/test', (req, res)=>{
     res.sendFile(__dirname + '/tmp/test.html');
 });
 
 app.use('/Users', usersRoutes);
+
+// const listPage = fs.readFileSync('list.ejs', 'utf8');
+app.get('/list', (req, res)=>{
+    db.query('SELECT * FROM users', (err, result) => {
+        if(err) console.error(err);
+        // console.log(result);
+        res.render('test.ejs', {data: result});
+    });
+});
+
+
 // 간편 시작하기
 // cd /d/Cansur/nodejsTest/src
 // nodemon server.js
