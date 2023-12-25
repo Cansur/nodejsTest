b835e84acc301eaebe935eaa437264e8c776e420
Cansur <tjswo0682@gmail.com>
2023-12-25 15:45:15 +0900

    라우터 알고리즘

---
+++ src/router/users.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+const db = require('../tmp/db');
+
+router.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '../tmp/users.html'));
+    // res.sendFile(__dirname + '../tmp/users.html'); 요놈은 안됨
+    db.query('SELECT * FROM users', function(err, results, fields) {
+        if(err) console.error(err);
+        console.log(results);
+        // res.json(results);
+    });
+});
+
+module.exports = router;
--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const db = require('../src/tmp/db.js');
+const db = require('../src/tmp/db');
 
+// Routers
+const usersRoutes = require('./router/users');
+
+// express
 const app = express();
-
 
 // configuration =================================================================
 app.listen(8080, function(){
@@ -13,17 +16,20 @@ app.get('/', function(요청, 응답){
     응답.sendFile(__dirname + '/index.html');
 });
 
-app.get('/users', (req, res)=>{
-    db.query('SELECT * FROM users', function(err, results, fields) {
-        if(err) console.error(err);
-        console.log(results);
-        res.json(results);
-    });
+// app.get('/users', (req, res)=>{
+//     res.sendFile(__dirname + '/tmp/users.html');
+//     db.query('SELECT * FROM users', function(err, results, fields) {
+//         if(err) console.error(err);
+//         console.log(results);
+//         // res.json(results);
+//     });
+// });
+
+app.get('/test', (req, res)=>{
+    res.sendFile(__dirname + '/tmp/test.html');
 });
 
-
-
-
+app.use('/Users', usersRoutes);
 // 간편 시작하기
 // cd /d/Cansur/nodejsTest/src
 // nodemon server.js
