10ad2d45dd22917db056762c67eaee9ccb2b1823
Cansur <tjswo0682@gmail.com>
2023-12-27 00:49:34 +0900

    html -> ejs 이전 완료

--- src/router/users.js
+++ src/router/users.js
@@ -4,7 +4,8 @@ const path = require('path');
 const db = require('./db');
 
 router.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '../tmp/users.html'));
+    // res.sendFile(path.join(__dirname, '../tmp/users.html'));
+    res.render('users');
     // res.sendFile(__dirname + '../tmp/users.html'); 요놈은 안됨
     db.query('SELECT * FROM users', function(err, results, fields) {
         if(err) console.error(err);
--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
+const http = require('http');
 const express = require('express');
-const fs = require('fs');
 
 // Routers
 const usersRoutes = require('./router/users');
@@ -7,27 +7,31 @@ const db = require('./router/db');
 
 // express
 const app = express();
+const server = http.createServer(app);
 
 // ejs
 app.set('view engine', 'ejs');
+// app.set('view', './views');
 
 // configuration =================================================================
-app.listen(8080, function(){
+server.listen(8080, function(){
     console.log('listening on port 8080');
 });
 
-app.get('/', function(요청, 응답){
-    응답.sendFile(__dirname + '/index.html');
+app.get('/', function(req, res){
+    // res.sendFile(__dirname + '/index.html');
+    res.render('index.ejs');
 });
 
-app.get('/test', (req, res)=>{
-    res.sendFile(__dirname + '/tmp/test.html');
+app.get('/test2', (req, res)=>{
+    // res.sendFile(__dirname + '/tmp/test.html');
+    res.render('test2.ejs');
 });
 
 app.use('/Users', usersRoutes);
 
 // const listPage = fs.readFileSync('list.ejs', 'utf8');
-app.get('/list', (req, res)=>{
+app.get('/test', (req, res)=>{
     db.query('SELECT * FROM users', (err, result) => {
         if(err) console.error(err);
         // console.log(result);
