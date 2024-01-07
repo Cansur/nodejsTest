fd195501ee257472be081e8c662ac7b2952aec00
Cansur <tjswo0682@gmail.com>
2024-01-08 04:19:25 +0900

    게시판 삭제 기능 추가

--- src/router/board.js
+++ src/router/board.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const db = require('./db');
+// const url = require('url');
 
 // var datas;
 
@@ -19,13 +20,28 @@ router.get(`/view/:idx`, function (req, res) {
     var sql2 = `UPDATE board SET likes = likes + 1 where seq = ${idx}`;
     db.query(sql, function (err, result, fields) {
         if (err) throw err;
+        result[0].contents = result[0].contents.replace(/(?:\r\n|\r|\n)/g, "<br />");
         res.render('boardView.ejs', { data : result});
     });
     db.query(sql2, function (err, result, fields) {
         if (err) throw err;
     });
-    // console.log(idx);
 });
 
+router.get(`/view/:idx/delete`, function (req, res) {
+    var idx = req.params.idx;
+    var sql = `delete from board where seq = ${idx}`;
+    db.query(sql, function (err, result, fields) {
+        if (err) throw err;
+        
+    });
+
+    return res.redirect("../../")
+    // console.log(res.url);
+    // global.location.href = "http://localhost:8080/board/";
+    // location = "http://localhost:8080/board/";
+    // location.href = "http://localhost:8080/board/";
+    // location.assign("http://localhost:8080/board/");
+});
 
 module.exports = router;
