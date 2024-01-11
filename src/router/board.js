cd93086deeb81aa09eeb7675c96598e374130ebe
Cansur <tjswo0682@gmail.com>
2024-01-11 18:08:08 +0900

    검색 기능 추가

--- src/router/board.js
+++ src/router/board.js
@@ -9,6 +9,47 @@ router.use(express.urlencoded({ extended: false }));
 
 
 router.get('/', function (req, res) {
+    var search = req.query.search;
+    // 페이징 처리
+    var countPerPage = req.query.countperpage;
+    var pageNo = req.query.pageno;
+    var pagePre = 0;
+    // 기본값 설정 및 전달 내용 확인
+    if (countPerPage == undefined || typeof countPerPage == "undefined" || countPerPage == null) {countPerPage = 10;}
+	else { countPerPage = parseInt(countPerPage); }
+	if (pageNo == undefined || typeof pageNo == "undefined" || pageNo == null) { pageNo = 1; } 
+    else { pageNo = parseInt(pageNo); }
+    if (search == undefined || typeof search == "undefined" || search == null) { search = ""; } 
+    // else { search = search; }
+
+    // PageNo 에 따라 분할
+    if(search == "") { sql = `select count(*) as 'number' from board`; }
+    else { sql = `select count(case when title like "%${search}%" then 1 end) as 'number' from board`;}
+    db.query(sql, function (err, result, fields) {
+        if (err) throw err;
+        
+        if (pageNo > 0) {
+            var totalCount = result[0].number; // 전체 크기
+            var startItemNo = ((pageNo-1) * countPerPage); // 시작 번호
+            var endItemNo = (pageNo * countPerPage); // 종료 번호
+            // 종료 번호가 전체 크기보다 크면 전체 크기로 변경
+            if (endItemNo > (totalCount - 1)) { endItemNo = totalCount; }
+            // console.log(Math.floor(pageNo/5))
+            pagePre = Math.floor((pageNo-1)/5) // 페이지 1, 6, 11 .... 간격 지정
+            var data2 = [totalCount, pageNo, pagePre, search]; // 변수 츄라이 츄라이
+            // console.log(totalCount, endItemNo); //
+            if(search == "") { sql = `select * from board order by seq desc limit ${startItemNo}, ${endItemNo - startItemNo}`; }
+            else { sql = `select * from board where title like "%${search}%" order by seq desc limit ${startItemNo}, ${endItemNo - startItemNo}`;} 
+            db.query(sql, function (err, result, fields) {
+                if (err) throw err;
+                res.render('board.ejs', {data : result, data2 : data2});
+            });
+        } 
+    });
+});
+
+router.post(`/`, function (req, res) {
+    var search = req.body.search // 검색 변수
     // 페이징 처리
     var countPerPage = req.query.countperpage;
     var pageNo = req.query.pageno;
@@ -19,10 +60,8 @@ router.get('/', function (req, res) {
 	if (pageNo == undefined || typeof pageNo == "undefined" || pageNo == null) { pageNo = 1; } 
     else { pageNo = parseInt(pageNo); }
 
-    
-
     // PageNo 에 따라 분할
-    sql = `select count(*) as 'number' from board`
+    sql = `select count(case when title like "%${search}%" then 1 end) as 'number' from board`
     db.query(sql, function (err, result, fields) {
         if (err) throw err;
         
@@ -31,25 +70,21 @@ router.get('/', function (req, res) {
             var startItemNo = ((pageNo-1) * countPerPage); // 시작 번호
             var endItemNo = (pageNo * countPerPage); // 종료 번호
             // 종료 번호가 전체 크기보다 크면 전체 크기로 변경
-            if (endItemNo > (totalCount - 1)) { endItemNo = totalCount - 1; }
-            // console.log(Math.floor(pageNo/5))
+            if (endItemNo > (totalCount - 1)) { endItemNo = totalCount; }
             pagePre = Math.floor((pageNo-1)/5) // 페이지 1, 6, 11 .... 간격 지정
-            var data2 = [totalCount, pageNo, pagePre]; // 변수 츄라이 츄라이
-            sql = `select * from board limit ${startItemNo}, ${endItemNo - startItemNo}`
+            var data2 = [totalCount, pageNo, pagePre, search]; // 변수 츄라이 츄라이
+
+            if(totalCount == 0) { 
+                res.render('board.ejs', {data : result, data2 : data2});
+            }
+            else{
+            sql = `select * from board where title like "%${search}%" order by seq desc limit ${startItemNo}, ${endItemNo - startItemNo}`;
             db.query(sql, function (err, result, fields) {
                 if (err) throw err;
                 res.render('board.ejs', {data : result, data2 : data2});
             });
+        }
         } 
-        // else {
-        //     // sql = `select * from board order by seq desc limit 10`
-        //     sql = `select * from board limit 10`
-        //     data2 = [result[0].number, pageNo, pagePre];
-        //     db.query(sql, function (err, result, fields) {
-        //         if (err) throw err;
-        //         res.render('board.ejs', {data : result , data2 : data2});
-        //     });
-        // }
     });
 });
 
